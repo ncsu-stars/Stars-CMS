3d5f43de6a554f84d6f7973589d7b1409498a16c
Eric Whitmire <emwhitmi@ncsu.edu>
2011-10-03 20:52:55 -0400

    Added blog tagging functionality

--- website/admin.py
+++ website/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from website.models import Project, Member, News, BlogPost
+from website.models import Project, Member, News, BlogPost, Tag
 
 admin.site.register(Project)
 admin.site.register(Member)
 admin.site.register(News)
 admin.site.register(BlogPost)
+admin.site.register(Tag)
 
 # Include this here for now
 
@@ -15,3 +16,4 @@ databrowse.site.register(Project)
 databrowse.site.register(Member)
 databrowse.site.register(News)
 databrowse.site.register(BlogPost)
+databrowse.site.register(Tag)
--- website/forms.py
+++ website/forms.py
@@ -1,11 +1,11 @@
 import floppyforms
 
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, ModelChoiceField
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
-from website.models import Member, Project, BlogPost
+from website.models import Member, Project, BlogPost, Tag
 
 class MemberForm(ModelForm):
 	class Meta:
@@ -16,7 +16,43 @@ class ProjectForm(ModelForm):
 	class Meta:
 		model = Project
 
+def func_concat(old_func, new_func):
+    def function():
+        old_func()
+        new_func()
+    return function
+
 class BlogForm(ModelForm):
+	tags2 = forms.CharField(max_length=255, label="Tags")
+	
 	class Meta:
 		model = BlogPost
-		exclude = ('author',)
+		exclude = ('author','tags')
+
+	def __init__(self, *args, **kwargs):
+		instance = kwargs.get('instance', None)
+		# See if we are editing an existing Blog. If not, there is nothing
+		# to be done.
+		if instance and instance.pk:
+			taglist = Tag.objects.filter(blogpost=instance).values_list('name', flat=True)
+			self.base_fields['tags2'].initial = ','.join(map(str, taglist))
+
+		super(BlogForm, self).__init__(*args, **kwargs)
+
+	def save(self, *args, **kwargs):       
+		commit = kwargs.get('commit', True)        
+		instance = super(BlogForm, self).save(*args, **kwargs)
+		
+		def save_m2m():
+			taglist = self.cleaned_data['tags2'].split(",")
+			instance.tags.clear()
+			for t in taglist:
+				tag = Tag.objects.get_or_create(name=t)[0]
+				instance.tags.add(tag)
+
+		if commit:
+			save_m2m()
+		else:
+			self.save_m2m = func_concat(self.save_m2m, save_m2m)
+
+		return instance
