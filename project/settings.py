a3c3496c899dca4003f8b3a8e3836e14ffc26727
Omar Estrella <oyestrel@ncsu.edu>
2011-10-30 17:35:55 -0400

    Changed to a more intuitive project and apps layout

--- project/settings.py
+++ project/settings.py
@@ -4,7 +4,7 @@ import os
 import django
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.join(BASE_DIR, "apps"))
+sys.path.insert(0, os.path.join(BASE_DIR, "../apps"))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
