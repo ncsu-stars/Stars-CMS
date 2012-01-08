52eddd11d070c978668b66df0df704b4477822d2
Mark Draelos <mtdraelo@ncsu.edu>
2012-01-08 00:33:27 -0500

    tweaks on forms disabled project coordinator editing of archived projects

--- apps/cms/permissions.py
+++ apps/cms/permissions.py
@@ -13,8 +13,8 @@ def can_user_create_project(user):
     return is_user_slc_leader(user)
 
 def can_user_edit_project(user, project):
-    # only project coordinators and the SLC leader can edit projects
-    # through the main interface
+    # only project coordinators (active/empty projects) and the SLC leader
+    # can edit projects through the main interface
     if user.is_anonymous():
         return False
 
@@ -22,7 +22,7 @@ def can_user_edit_project(user, project):
         return True
 
     try:
-        return project.is_member_coordinator(user.get_profile())
+        return project.status != Project.STATUS_ARCHIVED and project.is_member_coordinator(user.get_profile())
     except Member.DoesNotExist:
         return False
 
