63a97295be329fd56f907e799ab97efcf4c00825
Mark Draelos <mtdraelo@ncsu.edu>
2012-03-23 16:25:13 -0400

    implemented freezing of archived members, projects, and blogposts except for SLC leader access

--- apps/cms/permissions.py
+++ apps/cms/permissions.py
@@ -15,6 +15,7 @@ def can_user_create_project(user):
 def can_user_edit_project(user, project):
     # only project coordinators (active/empty projects) and the SLC leader
     # can edit projects through the main interface
+    # editing of archived projects is disallowed except for SLC leader
     if user.is_anonymous():
         return False
 
@@ -22,12 +23,13 @@ def can_user_edit_project(user, project):
         return True
 
     try:
-        return project.status != Project.STATUS_ARCHIVED and project.is_member_coordinator(user.get_profile())
+        return (project.status != Project.STATUS_ARCHIVED) and project.is_member_coordinator(user.get_profile())
     except Member.DoesNotExist:
         return False
 
 def can_user_delete_project(user, project):
     # only the SLC leader can delete projects through the main interface
+    # project status is not checked since the SLC leader user overrides
     return is_user_slc_leader(user)
 
 def can_user_demote_project_coordinators(user, project):
@@ -41,20 +43,24 @@ def can_user_create_member(user):
 def can_user_edit_member(user, member):
     # only the user that owns a member profile and the SLC leader can perform edits
     # through the main interface
-    return (user == member.user) or is_user_slc_leader(user)
+    # but only if that member is not archived for normal users (strictly not necessary since inactive user cannot log in)
+    return ((member.status != Member.STATUS_ARCHIVED) and (user == member.user)) or is_user_slc_leader(user)
 
 def can_user_delete_member(user, member):
     # only the SLC leader can delete members through the main interface
+    # member status is not checked since the SLC leader user overrides
     return is_user_slc_leader(user)
 
 def can_user_post_as_member(user, member):
     # only the user that owns a member profile can post to that member's blog
-    return (user == member.user)
+    # but only if that member is not archived for normal users (strictly not necessary since inactive user cannot log in)
+    return ((member.status != Member.STATUS_ARCHIVED) and (user == member.user))
 
 def can_user_edit_blogpost(user, blogpost):
     # only blogpost authors and the SLC leader can edit blogposts
     # through the main interface
-    return (user == blogpost.author.user) or is_user_slc_leader(user)
+    # but only if that member is not archived for normal users (strictly not necessary since inactive user cannot log in)
+    return ((blogpost.author.status != Member.STATUS_ARCHIVED) and (user == blogpost.author.user)) or is_user_slc_leader(user)
 
 def can_user_create_page(user):
     # only the SLC leader can create pages through the main interface
