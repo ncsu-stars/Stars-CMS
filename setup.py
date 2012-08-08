1dea2f6f0daeb665b98cf7c30aa2e432513a00f4
Omar Estrella <gzusfr3ak@gmail.com>
2012-08-08 19:52:05 -0400

    Add packaging files; remove apps directory

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+    version='1.0.1',
+    description='Python Distribution Utilities',
+    author='NCSU STARS',
+    author_email='',
+    url='https://github.com/ncsu-stars/Stars-CMS/',
+    packages=['distutils', 'distutils.command'],
+)
