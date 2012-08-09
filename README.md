f4fba989edc7971c7bb1faa028b9f6956c9e4845
Omar Estrella <gzusfr3ak@gmail.com>
2012-08-08 21:38:29 -0400

    Final release prep and edits

--- README.md
+++ README.md
@@ -6,6 +6,14 @@ This is a CMS written in Django specifically for STARS SLC's. This CMS is used a
 Installation
 ------------
 
+### App Installation
+
+There are two solutions:
+
+1. Download the application by running `https://github.com/ncsu-stars/Stars-CMS.git` in your project directory. 
+
+2. Run `pip install stars-cms` and add `cms` to your `INSTALLED_APPS`
+
 ### Create your virtual environment
 
 Make sure you have pip and virtualenv installed on your system already
@@ -14,6 +22,10 @@ Make sure you have pip and virtualenv installed on your system already
   2. Run the following: ``virtualenv --no-site-packages --distribute ve``
   3. Active the virtual environment by running: ``source ve/bin/activate``
   4. Install the requirements by running: ``pip install -r requirements.txt``
+
+### Settings
+
+Take a look at `settings.py.example` for example project settings
 
 ### Run your server
 
--- setup.py
+++ setup.py
@@ -1,12 +1,38 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import os
+from distutils.core import setup, find_packages
 
-setup(name='Distutils',
-    version='1.0.1',
-    description='Python Distribution Utilities',
+
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
+
+
+setup(name='STARS CMS',
+    version='1.0.1b1',
     author='NCSU STARS',
     author_email='',
     url='https://github.com/ncsu-stars/Stars-CMS/',
-    packages=['distutils', 'distutils.command'],
+    license='BSD',
+    description='CMS written in Python/Django specifically for STARS SLC\'s',
+    packages=find_packages(exclude=['ncsu', 'project']),
+    classifiers=[
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Framework :: Django',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+    ],
+    long_description=read_file('README.rst'),
+    zip_safe=False,  # because we're including media that Django needs
 )
